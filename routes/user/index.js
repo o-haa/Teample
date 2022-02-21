ab5248f4c5f707f5f3bc722b4622b147cdc864d8
o-haa <haeun9627@hanmail.net>
2022-02-21 16:17:57 +0900

    user 기본 css 추가

--- routes/user/index.js
+++ routes/user/index.js
@@ -5,4 +5,12 @@ router.get('/login', (req, res)=>{
     res.render('./user/login.html')
 })
 
+router.get('/join', (req, res)=>{
+    res.render('./user/join.html')
+})
+
+router.get('/profile', (req, res)=>{
+    res.render('./user/profile.html')
+})
+
 module.exports = router;
