b98333f3fcf5edc9846f8c304f23884a20a03e81
bitkunst <bitkunst@Romantiker.localdomain>
2022-02-22 16:45:18 +0900

    add admin grant

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('./admin.controller.js')
 const {Auth} = require('../../util/auth.js')
+const {adminAuth} = require('../../util/admin_auth.js')
 
 
 router.get('/', adminController.admin)
@@ -12,9 +13,11 @@ router.post('/login', adminController.login)
 
 router.get('/logout', Auth, adminController.logout)
 
-router.get('/list', Auth, adminController.adminList)
+router.get('/list', Auth, adminAuth, adminController.adminList)
 
-router.get('/info', Auth, adminController.adminInfo)
+router.get('/info', Auth, adminAuth, adminController.adminInfo)
+
+router.post('/info', adminController.postAdminInfo)
 
 // router.get('/update', adminController.getUpdate)
 
